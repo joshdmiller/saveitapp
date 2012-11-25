b7eba7379be7d32432a876692eb7056f9df675b7
Josh David Miller <josh@joshdmiller.com>
2012-11-24 20:16:41 -0800

    Added basic JSON framework

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,36 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 object Application extends Controller {
   
   def index = Action {
     Ok("Save It App Home").as("text/html")
+  }
+
+  def get_user(id: Long) = Action {
+    // FIXME: static user matching for development
+    id match {
+      case 1234 => Ok(
+        JsObject(
+          "status"  -> JsNumber(200) ::
+          "message" -> JsString("Success") ::
+          "user"    -> JsObject(
+            "id"      -> JsNumber(id) ::
+            Nil
+          ) ::
+          Nil
+        )
+      )
+      case _ => NotFound(
+        JsObject(
+          "status" -> JsNumber(404) ::
+          "message" -> JsString("User ["+id+"] not found.") ::
+          Nil
+        )
+      )
+    }
   }
   
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -4,6 +4,7 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json._
 
 /**
  * Add your spec here.
@@ -28,6 +29,30 @@ class ApplicationSpec extends Specification {
         contentType(home) must beSome.which(_ == "text/html")
         contentAsString(home) must contain ("Save It App Home")
       }
+    }
+
+    "return JSON from API call: GET /users/{id}" in {
+      val apiCall = routeAndCall(FakeRequest(GET, "/users/1234"))
+      apiCall must not be none
+
+      val  result = apiCall.get
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "application/json")
+      
+      val json = Json.parse(contentAsString(result))
+      val code = (json \ "status").asOpt[Int]
+      code must not be none
+      code.get mustEqual OK
+    }
+
+    "return valid user data from API call: GET /users/{id}" in {
+      val result = routeAndCall(FakeRequest(GET, "/users/1234")).get
+      val json = Json.parse(contentAsString(result))
+      
+      // ensure we have an id
+      val id = (json \ "user" \ "id").asOpt[Int]
+      id must not be none
+      id.get mustEqual 1234
     }
   }
 }
